30e9c6591656aa67f665702b2406b761a621ca88
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2025-10-31 18:59:59 +0100

    M2-g6-lab project created and EX1 added

---
+++ Assets/Project/EX1/Scripts/VariableSwap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableSwap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a, b, c; // dichiaro le variabili
+
+        a = 10; // assegno il valore inziale di a
+        b = 20; // assegno il valore iniziale di b
+
+        Debug.Log("Il valore della variabile a = " + a + " mentre il valore della Variabile b = " + b);
+
+
+        Debug.Log("Eseguo lo swap dei valori a e b usando un'altra variabile di appoggio c !!");
+
+        c = b; // salvo valore di b in una variabile di transito.
+        b = a; // assegno a b il valore di a.
+        a = c; // assegno  ad a il valore che era di b. 
+
+        Debug.Log("Il valore della variabile a = " + a + " mentre il valore della Variabile b = " + b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

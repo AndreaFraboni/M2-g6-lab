2cfd479551a2b602cc953c235984216eb8f98d80
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2025-11-05 20:29:55 +0100

    EX3 updated

--- Assets/Project/EX3/Scripts/EX3.cs
+++ Assets/Project/EX3/Scripts/EX3.cs
@@ -6,18 +6,19 @@ public class EX3 : MonoBehaviour
 {
     [SerializeField] private int dividendo;
     [SerializeField] private int divisore;
-    [SerializeField] private int risultato;
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        int risultato;
+
         if (SafeDivide(dividendo, divisore, out risultato))
         {
             Debug.Log("La divisione si può fare ed il risultato è = " + risultato);
         }
         else
         {
-            Debug.Log("WARNING : IL DIVISORE NON PUO' ESSERE ZERO !!!!!");
+            Debug.LogWarning("IL DIVISORE NON PUO' ESSERE ZERO !!!!!");
         }
     }
 
